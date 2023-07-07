a7abfba3e14d78aea28936a2915657be8d514257
rt-rocket <108962288+rt-rocket@users.noreply.github.com>
2023-07-06 22:20:20 -0500

    Update tabs router with new queries

---
+++ src/server/api/routers/tabs.ts
@@ -0,0 +1,45 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  privateProcedure,
+} from "~/server/api/trpc";
+
+export const tabsRouter = createTRPCRouter({
+  getAll: privateProcedure.query(async ({ ctx }) => {
+    const currentUserId = ctx.userId;
+
+    try {
+      return await ctx.prisma.tab.findMany({
+        where: {
+          user_id: currentUserId,
+        },
+      });
+    } catch (error) {
+      console.log("error", error);
+      throw new Error("Error getting tabs");
+    }
+  }),
+
+  create: privateProcedure
+    .input(
+      z.object({
+        url: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const currentUserId = ctx.userId;
+
+      try {
+        await ctx.prisma.tab.create({
+          data: {
+            user_id: currentUserId,
+            url: input.url,
+          },
+        });
+      } catch (error) {
+        console.log(error);
+        throw new Error("Error creating tab");
+      }
+    }),
+});
