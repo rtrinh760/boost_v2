b7c4dbab48b22c7a7fe75ce4e2e88d51f0d7640e
rt-rocket <108962288+rt-rocket@users.noreply.github.com>
2023-07-06 22:19:19 -0500

    Add form component

---
+++ src/components/form.tsx
@@ -0,0 +1,46 @@
+import { useAuth } from "@clerk/nextjs";
+import { useState } from "react";
+import { api } from "~/utils/api";
+
+const Form = () => {
+  const [input, setInput] = useState("");
+  const { isSignedIn } = useAuth();
+
+  const ctx = api.useContext();
+
+  const { mutate, isLoading } = api.tabs.create.useMutation({
+    onSuccess: () => {
+      void ctx.tabs.getAll.invalidate();
+    },
+  });
+
+  if (!isSignedIn) {
+    return null;
+  }
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        mutate({ url: input });
+        setInput("");
+      }}
+    >
+      <input
+        type="text"
+        className="border-2 border-zinc-800 bg-neutral-900 px-4 py-2 text-white focus:outline-none"
+        placeholder="Enter URL"
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+      />
+      <button
+        type="submit"
+        className="border-2 border-zinc-800 bg-[#26263d] p-2 text-white focus:outline-none"
+      >
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Form;
