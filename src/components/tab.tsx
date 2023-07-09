b55d4f6df316a826029ceb2526c7203b9e414d6c
rt-rocket <108962288+rt-rocket@users.noreply.github.com>
2023-07-09 17:33:35 -0500

    Add short url for favicon and link name

--- src/components/tab.tsx
+++ src/components/tab.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import getWebsiteTitle from "~/scripts/getWebsiteTitle";
+
 export type Tab = {
   url: string;
   id: string;
@@ -8,12 +11,30 @@ export type Tab = {
 
 const Tab = (props: Tab) => {
   const tab = props;
+  const shortUrl = tab.url
+    .replace(/^(?:https?:\/\/)?(?:www\.)?/i, "")
+    .split("/")[0];
+  
   return (
     <div
       key={tab.id}
       className="flex flex-col items-center justify-center px-20 text-center"
     >
-      <div className="p-2 text-white">{tab.url}</div>
+      <div className="p-2 text-white">
+        <Link
+          href={`${tab.url}`}
+          target="_blank"
+          rel="noopener"
+          className="flex flex-row space-x-2"
+        >
+          <img
+            height="16"
+            width="24"
+            src={`http://www.google.com/s2/favicons?domain=${shortUrl}`}
+          />
+          <span>{shortUrl}</span>
+        </Link>
+      </div>
     </div>
   );
 };
