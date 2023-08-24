ac68f5d4d46ec316917fe5808ec438d6ba94f5fd
rt-rocket <108962288+rt-rocket@users.noreply.github.com>
2023-08-23 19:10:53 -0500

    Update form styling, add check for duplicate tab

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -13,14 +13,18 @@ const Form = ({ sessionId }: FormProps) => {
 
   const ctx = api.useContext();
 
+  const { data } = api.tabs.getAll.useQuery({
+    session_id: sessionId,
+  });
+
   const { mutate } = api.tabs.create.useMutation({
     onSuccess: () => {
       toast.success("Tab saved!");
       void ctx.tabs.getAll.invalidate();
     },
     onError: () => {
-      toast.error("URL is invalid. Please try again.");
-    }
+      toast.error("Failed to save tab. Please try again.");
+    },
   });
 
   if (!isSignedIn) {
@@ -32,25 +36,51 @@ const Form = ({ sessionId }: FormProps) => {
       onSubmit={(e) => {
         e.preventDefault();
 
-        mutate({  session_id: sessionId, url: input.toLowerCase() });
+        let tabExists = false;
+
+        for (const tab of data || []) {
+          if (tab.url.toLowerCase() === input.toLowerCase()) {
+            tabExists = true;
+          }
+        }
+
+        if (!tabExists) {
+          mutate({ session_id: sessionId, url: input.toLowerCase() });
+        } else {
+          toast.error("Tab already exists!");
+        }
+
         setInput("");
       }}
-      className="pl-40 group">
+      className="group relative flex w-[300px] flex-1 flex-row sm:w-[450px]"
+    >
       <input
         type="text"
-        className=" bg-[#fcfbfe] align-middle px-4 py-2 text-[#0a0510] focus:outline-none"
+        className=" bg-[#fcfbfe] px-4 py-2 align-middle text-[#0a0510] focus:outline-none"
         placeholder="Enter URL"
         value={input}
         onChange={(event) => setInput(event.target.value)}
       />
       <button
         type="submit"
-        className="p-2 align-middle absolute text-zinc-400 focus:outline-none"
+        className="flex-end absolute right-3 hidden translate-y-2 font-bold hover:text-[#0a0510] group-hover:block group-hover:text-[#0a0510]"
       >
-        <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-</svg>
-</span>
+        <span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-5 w-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 4.5v15m7.5-7.5h-15"
+            />
+          </svg>
+        </span>
       </button>
     </form>
   );
